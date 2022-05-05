a5d2ea1aaf20ec21777cfb9d484a0fdb81f93622
Gabriel Comte <comte@puzzle.ch>
2022-05-05 13:32:06 +0200

    Add data

---
+++ data.js
@@ -0,0 +1,38 @@
+new svgMap({
+    targetElementID: 'svgMap',
+    colorNoData: "#555",
+    colorMin: "#f2a900",
+    colorMax: "#f2a900",
+    noDataText: "NGMI",
+    data: {
+        data: {
+            gdp: {
+                name: 'GDP',
+                format: '${0} billion',
+                thousandSeparator: ',',
+                thresholdMax: 50000,
+                thresholdMin: 1000
+            },
+            // change: {
+            //     name: 'Change to year before',
+            //     format: '{0} %'
+            // },
+            population: {
+                name: 'Population',
+                format: '{0}',
+                thousandSeparator: ',',
+                thresholdMax: 50000,
+                thresholdMin: 1000
+            },
+            date: {
+                name: 'Bitcoin Legal Tender',
+                format: 'since: {0}',
+            }
+        },
+        applyData: 'gdp',
+        values: {
+            SV: {gdp: 57.95, population: 6830000, date: "Sep 07, 2021"},
+            CF: {gdp: 4.262, population: 4666368, date: "April 28, 2022"},
+        }
+    }
+});
